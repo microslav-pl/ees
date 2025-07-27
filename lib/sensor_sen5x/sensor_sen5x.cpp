b646558b683af654504a2e656a64bbdfe4b23000
Krzysztof Daszuta <irukard@gmail.com>
2025-07-27 14:46:20 +0200

    Sensirion SEN5x simple scheduler

--- lib/sensor_sen5x/sensor_sen5x.cpp
+++ lib/sensor_sen5x/sensor_sen5x.cpp
@@ -18,6 +18,8 @@ static uint32_t sen5x_pm2p5_index = 0;
 static uint32_t sen5x_pm4_index = 0;
 static uint32_t sen5x_pm10_index = 0;
 
+static uint32_t sen5x_lastMeasurementTime = 0;
+
 bool sensorInitSen5x() {
     return true;
 }
@@ -31,7 +33,15 @@ bool sensorReadSen5x() {
 }
 
 void scheduleReadSen5x() {
-    sensorReadSen5x();
+    if (millis() - sen5x_lastMeasurementTime < SEN5X_READ_INTERVAL) {
+        return;
+    }
+    sen5x_lastMeasurementTime = millis();    
+    for (uint8_t i=0; i < SEN5X_READ_RETRY; i++) {
+        if (sensorReadSen5x()) {
+            break;
+        }
+    }
 }
 
 float medianPM1Sen5x() {
