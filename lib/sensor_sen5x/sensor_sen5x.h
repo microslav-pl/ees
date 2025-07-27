618430725d21e943e8e44327001708251f7d7ef1
Krzysztof Daszuta <irukard@gmail.com>
2025-07-27 15:11:09 +0200

    Sensirion SEN5x doxygen

--- lib/sensor_sen5x/sensor_sen5x.h
+++ lib/sensor_sen5x/sensor_sen5x.h
@@ -1,15 +1,46 @@
 #pragma once
 
+/**
+ * @brief Initializes Sensirion SEN5x.
+ * @return Status of initialization.
+ * @retval true Initialization was successful.
+ * @retval false Initialization failed.
+ */
 bool sensorInitSen5x();
 
+/**
+ * @brief Reads data from Sensirion SEN5x.
+ * @return Status of operation.
+ * @retval true Read was successful.
+ * @retval false Read failed.
+ */
 bool sensorReadSen5x();
 
+/**
+ * @brief Simple scheduler for reading Sensirion SEN5x.
+ */
 void scheduleReadSen5x();
 
+/**
+ * @brief Returns median PM 1.0 form ring buffer.
+ * @return PM 1.0 (µg/m³).
+ */
 float medianPM1Sen5x();
 
+/**
+ * @brief Returns median PM 2.5 form ring buffer.
+ * @return PM 2.5 (µg/m³).
+ */
 float medianPM2_5Sen5x();
 
+/**
+ * @brief Returns median PM 4.0 form ring buffer.
+ * @return PM 4.0 (µg/m³).
+ */
 float medianPM4Sen5x();
 
+/**
+ * @brief Returns median PM 10.0 form ring buffer.
+ * @return PM 10.0 (µg/m³).
+ */
 float medianPM10Sen5x();
